b8cb9ac51ad29880418a1ba44466ffb9d79bccd3
Vyacheslav Shaganov <slavikshag@gmail.com>
2023-10-24 11:27:19 +0300

    visualization corrected for case of 1 station

--- visualize/designations.py
+++ visualize/designations.py
@@ -10,34 +10,43 @@ class DesignationsDrawer:
         filedata = self.file.read().split('\n')
         
         data = {}
-        t_group = None
 
-        def append(id, t,dist,azm,ang):
-            data[id]['time'].append(t)
-            data[id]['dist'].append(dist)
-            data[id]['azimuth'].append(azm)
-            data[id]['angle'].append(ang)
+        def append(id, t,dist,azm,ang, index=-1):
+            if index == -1:
+                data[id]['time'].append(t)
+                data[id]['dist'].append(dist)
+                data[id]['azimuth'].append(azm)
+                data[id]['angle'].append(ang)
+            else:
+                data[id]['time'].insert(index, t)
+                data[id]['dist'].insert(index, dist)
+                data[id]['azimuth'].insert(index, azm)
+                data[id]['angle'].insert(index, ang)
+
+        dt = 1e9
 
         for line in filedata[1:-1]:
             t,id,dist,azm,ang = [float(a) for a in line.split('\t')]
-            
             id = int(id)
+
             if id not in data:
-                data[id] = {'time': [], 'dist': [], 'azimuth': [], 
-                            'angle': [], 'was': False}
-            if not data[id]['was']:
-                append(id, *[np.nan]*4)
-            if len(data[id]['time']) > 2 and \
-                abs(data[id]['azimuth'][-1] - data[id]['azimuth'][-2]) > 18 / np.pi:
-                data[id]['azimuth'][-1] = np.nan
-            if t != t_group:
-                for _id in data:
-                    if data[_id]['time'][-1] != t_group:
-                        data[_id]['was'] = False
-                t_group = t
+                data[id] = {'time': [], 'dist': [], 
+                            'azimuth': [], 'angle': []}
+            if data[id]['time']:
+                dt = min(dt, t - data[id]['time'][-1])
+            
             append(id, t,dist,azm,ang)
-            data[id]['was'] = True
         
+        # include discontinuties
+        for id in data:
+            t_idx = len(data[id]['time']) - 1
+            while t_idx != 1:
+                if data[id]['time'][t_idx] - data[id]['time'][t_idx-1] != dt:
+                    append(id, *[np.nan]*4, t_idx)
+                elif abs(data[id]['azimuth'][t_idx] - data[id]['azimuth'][t_idx-1]) > 18 / np.pi:
+                    data[id]['azimuth'][t_idx] = np.nan
+                t_idx -= 1
+
         for id in data:
             t = np.array(data[id]['time']) / 3600.0 
             fig, axs = plt.subplots(3, sharex=True)
